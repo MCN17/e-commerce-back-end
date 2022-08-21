1da763c5ae2548a386c4efa8f424974bf8de5dbb
Marty <martynolan17@gmail.com>
2022-08-21 09:13:02 -0400

    added get/delete routes for product

--- models/index.js
+++ models/index.js
@@ -5,15 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Category.belongsTo(Product, {
-  foreignKey: "product_id",
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
 });
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+through: ProductTag,
+foreignKey: "product_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+})
 
 module.exports = {
   Product,
